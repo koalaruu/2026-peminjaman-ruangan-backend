608acb645d3ff098ba54003fde7ca5af0b1b0d62
koalaruu <koalaruu@gmail.com>
2026-02-16 19:21:11 +0700

    chore(cors): allow Vite alternate dev port (http://localhost:5174)

--- RoomBookingApp.Api/Program.cs
+++ RoomBookingApp.Api/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ApiDbContext>(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
-        policy => policy.WithOrigins("http://localhost:5173") // Port Vite kamu
+        policy => policy.WithOrigins("http://localhost:5173", "http://localhost:5174") // Vite dev may pick different port
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
